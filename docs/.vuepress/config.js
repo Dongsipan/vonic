dc39da9bd1e5ec39c4cbfb99619d1028d8a045b9
dongsipan <75098051@qq.com>
2019-07-11 11:05:00 +0800

    add button md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
   title: 'Spirit UI',
   description: 'Spirit UI',
@@ -9,10 +10,30 @@ module.exports = {
     sidebarDepth: 2,
     lastUpdated: 'Last Updated',
     nav: [
+      { text: 'Home', link: '/' },
       {
         text: 'GitHub',
-        link: 'https://github.com/Dongsipan/spirit-ui'
+        link: 'https://github.com/Dongsipan/spirit'
       }, 
+    ],
+    sidebar: [
+      {
+        title: '组件',
+        collapsable: true,
+        children: [
+          'views/components/basic/'
+        ]
+      },
     ]
-  }
+  },
+  postcss: [require('autoprefixer')],
+    sass: { indentedSyntax: true },
+    scss: {
+        includePaths: ["../../components/style"]
+    },
+    chainWebpack: (config, isServer) => {
+        config.resolveLoader
+            .modules
+            .add(path.resolve(__dirname, './node_modules'))
+    }
 }
---
+++ docs/views/components/basic/README.md
@@ -0,0 +1,19 @@
+---
+title: 'Basic 基础'
+sidebarDepth: 2
+---
+
+
+
+## Button 按钮
+<ClientOnly>
+  <spirit-button/>
+<font size=5>Attributes</font>
+| 参数| 说明 | 类型 | 可选值 | 默认值 |
+| :------ | ------ | ------ | ------ | ------ |
+| type | 按钮类型 | string |primary, secondary| - |
+| disabled | 按钮是否禁用 | boolean |- | false |
+| icon | 按钮上图标名称 | string |- | - |
+| loading | 显示加载中图标 | boolean |- | false |
+
+</ClientOnly>
