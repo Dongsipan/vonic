9f704d0101b8d3c7855e5ec25f330614269d2534
董思盼 <dongsipan@dongsipandeMacBook-Pro.local>
2019-07-17 20:19:37 +0800

    modefy scroll

--- docs/.vuepress/components/spirit-button.vue
+++ docs/.vuepress/components/spirit-button.vue
@@ -63,7 +63,47 @@ export default {
   data() {
     return {
       code1: `
-       `
+       <von-button color="primary">Primary</von-button>
+        <von-button color="secondary">Secondary</von-button>
+        <von-button color="tertiary">Tertiary</von-button>
+        <von-button color="success">Success</von-button>
+        <von-button color="warning">Warning</von-button>
+        <von-button color="danger">Danger</von-button>
+        <von-button color="light">Light</von-button>
+        <von-button color="medium">Medium</von-button>
+        <von-button color="dark">Dark</von-button>
+
+        <!-- Expand -->
+        <von-button expand="full">Full Button</von-button>
+        <von-button expand="block">Block Button</von-button>
+
+        <!-- Round -->
+        <von-button shape="round">Round Button</von-button>
+
+        <!-- Fill -->
+        <von-button expand="full" fill="outline">Outline + Full</von-button>
+        <von-button expand="block" fill="outline">Outline + Block</von-button>
+        <von-button shape="round" fill="outline">Outline + Round</von-button>
+
+        <!-- Icons -->
+        <!-- <von-button>
+          <von-icon slot="start" name="star"></von-icon>
+          Left Icon
+        </von-button>
+
+        <von-button>
+          Right Icon
+          <von-icon slot="end" name="star"></von-icon>
+        </von-button>
+
+        <von-button>
+          <von-icon slot="icon-only" name="star"></von-icon>
+        </von-button> -->
+
+        <!-- Sizes -->
+        <von-button size="large">Large</von-button>
+        <von-button>Default</von-button>
+        <von-button size="small">Small</von-button>`
         .replace(/^\s*/gm, '')
         .trim()
     }
